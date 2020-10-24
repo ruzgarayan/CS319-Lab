16146f552d9b6db34b5946a5f3bff0bfe91568f7
Ruzgar <zzlawlzz52@gmail.com>
2020-10-24 13:26:42 +0300

    add SOE optimization

--- src/PrimeGenerator.java
+++ src/PrimeGenerator.java
@@ -1,7 +1,26 @@
 import java.util.ArrayList;
 
 public class PrimeGenerator {
-    
+
+    private boolean prime[];
+    private final int soeLimit = 1000000;
+
+    public PrimeGenerator() {
+        this.prime = new boolean[this.soeLimit+1];
+        for(int i = 2; i < this.soeLimit; i++)
+            prime[i] = true;
+        sieveOfEratosthenes(this.soeLimit);
+    }
+
+    private void sieveOfEratosthenes(int n) {
+        for(int p = 2; p * p <= n; p++) {
+            if(prime[p]) {
+                for(int i = p * 2; i <= n; i += p)
+                    prime[i] = false;
+            }
+        }
+    }
+
     public ArrayList<Integer> generatePrimes(int limit) {
         ArrayList<Integer> primes = new ArrayList<Integer>();
 
@@ -12,10 +31,11 @@ public class PrimeGenerator {
 
         return primes;
     }
-
+    
     private boolean isPrime(int number) {
-        if (number <= 1)
-            return false;
+        if (number <= this.soeLimit) {
+            return prime[number];
+        }
 
         for (int i = 2; i <= Math.sqrt(number); i++) {
             if (number % i == 0)
